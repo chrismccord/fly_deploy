895322ee9173a4145e453d04ff9bbd7f98867dd8
Chris McCord <chris@chrismccord.com>
2025-11-04 12:21:00 -0500

    Release 0.1.0

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule FlyDeploy.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "FlyDeploy",
+      package: package(),
+      homepage_url: "https://github.com/chrismccord/fly_deploy",
+      description: """
+      Hot code upgrades for Elixir applications running on Fly.io
+      """,
     ]
   end
 
@@ -23,6 +29,17 @@ defmodule FlyDeploy.MixProject do
     [
       {:req, "~> 0.5"},
       {:finch, "~> 0.20"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Chris McCord"],
+      licenses: ["MIT"],
+      links: %{
+        GitHub: "https://github.com/chrismccord/fly_deploy"
+      },
+      files: ~w(lib CHANGELOG.md LICENSE.md mix.exs README.md .formatter.exs)
     ]
   end
 end
